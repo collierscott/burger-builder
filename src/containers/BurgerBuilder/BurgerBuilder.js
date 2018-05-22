945293a599ef4b06b2c47de1071068b27f203ef8
Scott Collier <scott@onlinespaces.com>
2018-05-22 20:45:43 +0200

    Should be able to read ingredients from array

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,24 @@ import AuxWrapper from '../../hoc/AuxWrapper';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {...}
+    // }
+
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+        }
+    };
+
     render () {
         return (
             <AuxWrapper>
-                <Burger/>
+                <Burger ingredients={this.state.ingredients}/>
                 <div>Builder Controls</div>
             </AuxWrapper>
         );
