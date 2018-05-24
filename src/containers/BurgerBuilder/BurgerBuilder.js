9303fadfca29361f5d429398a111afb1cde0aaa6
Scott Collier <scott.collier@umuc.edu>
2018-05-24 12:00:29 +0200

    Add controls to the builder and display to the user

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import AuxWrapper from '../../hoc/AuxWrapper';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
     // constructor(props) {
@@ -21,7 +22,7 @@ class BurgerBuilder extends Component {
         return (
             <AuxWrapper>
                 <Burger ingredients={this.state.ingredients}/>
-                <div>Builder Controls</div>
+                <BuildControls/>
             </AuxWrapper>
         );
     }
