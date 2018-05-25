6309b257fcc434b8d77065ccb91e9865bf22a48b
Scott Collier <scott.collier@umuc.edu>
2018-05-25 09:23:39 +0200

    Prevent order summary from updating when not needed

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Modal.css';
 import AuxWrapper from '../../../hoc/AuxWrapper';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => {
-    return (
-        <AuxWrapper>
-            <Backdrop show={props.show} clicked={props.modalClosed}/>
-            <div
-                className={classes.Modal}
-                style={{
-                    transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                    opacity: props.show ? '1' : '0'
-                }}
-            >
-                {props.children}
-            </div>
-        </AuxWrapper>
-    )
-};
+class Modal  extends Component {
+    // The wrapping class controls update of children.
+    // Converted to a class to control when it is updated.
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show;
+    }
 
-export default modal;
+    render() {
+        return (
+            <AuxWrapper>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
+                <div
+                    className={classes.Modal}
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1' : '0'
+                    }}
+                >
+                    {this.props.children}
+                </div>
+            </AuxWrapper>
+        )
+    }
+}
+
+export default Modal;
