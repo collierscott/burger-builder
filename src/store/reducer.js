1088dc21612eb3cca614ec30dbe3af008045bf63
Scott Collier <scott@onlinespaces.com>
2018-05-28 12:33:34 +0200

    Objects should be updated immutably using a utility function

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,4 +1,5 @@
 import * as actionTypes from './actions/index';
+import { updateObject} from "./utility";
 
 const initialState = {
     ingredients: {
@@ -21,23 +22,17 @@ const INGREDIENT_PRICES = {
 const reducer = ( state = initialState, action ) => {
     switch ( action.type ) {
         case actionTypes.ADD_INGREDIENT:
-            return {
-                ...state,
-                ingredients: {
-                    ...state.ingredients,
-                    [action.ingredientName]: state.ingredients[action.ingredientName] + 1
-                },
+            return updateObject(state, {
+                ingredients: updateObject(state.ingredients, {[action.ingredientName]: state.ingredients[action.ingredientName] + 1}),
                 totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
-            };
+            });
         case actionTypes.REMOVE_INGREDIENT:
-            return {
-                ...state,
-                ingredients: {
-                    ...state.ingredients,
+            return updateObject(state, {
+                ingredients: updateObject(state.ingredients, {
                     [action.ingredientName]: state.ingredients[action.ingredientName] - 1
-                },
+                }),
                 totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
-            };
+            });
         default:
             return state;
     }
