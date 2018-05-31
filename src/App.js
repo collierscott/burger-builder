9fabb9009b2ca0b388557c538a9f0b090bc7d89c
Scott Collier <scott.collier@umuc.edu>
2018-05-31 10:22:31 +0200

    User should be automatically logged in if token has not expired

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, withRouter } from 'react-router-dom';
+import {connect} from 'react-redux';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
 import Logout from "./containers/Auth/Logout/Logout";
+import * as action from './store/actions/index';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <Layout>
-          <Switch>
-            <Route path="/checkout" component={Checkout} />
-            <Route path="/orders" component={Orders} />
-            <Route path="/login" component={Auth} />
-            <Route path="/logout" component={Logout} />
-            <Route path="/" component={BurgerBuilder} />
-          </Switch>
-        </Layout>
-      </div>
-    );
-  }
+
+    componentDidMount() {
+        this.props.onTryAutoSignup();
+    }
+
+    render() {
+        return (
+          <div>
+            <Layout>
+              <Switch>
+                <Route path="/checkout" component={Checkout} />
+                <Route path="/orders" component={Orders} />
+                <Route path="/login" component={Auth} />
+                <Route path="/logout" component={Logout} />
+                <Route path="/" component={BurgerBuilder} />
+              </Switch>
+            </Layout>
+          </div>
+        );
+    }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => {
+    return {
+        onTryAutoSignup: () => dispatch(action.authCheckState())
+    }
+};
+
+export default withRouter(connect(null, mapDispatchToProps)(App));
--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -20,5 +20,6 @@ export {
 export {
     auth,
     logout,
-    setAuthRedirectPath
+    setAuthRedirectPath,
+    authCheckState
 } from './auth';
