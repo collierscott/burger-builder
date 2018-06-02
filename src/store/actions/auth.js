70e5e341366956ec6e7239dc519131f6c2730c7f
Scott Collier <scott@onlinespaces.com>
2018-06-02 14:42:43 +0200

    Install redux-saga and create a logout saga

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -31,11 +31,11 @@ export const checkAuthTimeout = (expirationTime) => {
 };
 
 export const logout = () => {
-    localStorage.removeItem('token');
-    localStorage.removeItem('expirationDate');
-    localStorage.removeItem('userId');
+    // localStorage.removeItem('token');
+    // localStorage.removeItem('expirationDate');
+    // localStorage.removeItem('userId');
     return {
-        type: actionTypes.AUTH_LOGOUT
+        type: actionTypes.AUTH_INITIATE_LOGOUT
     };
 };
 
